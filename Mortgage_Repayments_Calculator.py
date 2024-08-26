b5d58d3860951581ab32fdc196b904a2c20ba709
Sadiaakter12 <158366648+Sadiaakter12@users.noreply.github.com>
2024-08-25 21:22:35 -0400

    Update Mortgage_Repayments_Calculator.py

--- Mortgage_Repayments_Calculator.py
+++ Mortgage_Repayments_Calculator.py
@@ -7,7 +7,7 @@ st.title("Mortgage Repayments Calculator")
 
 st.header("Data Input")
 col1, col2 = st.columns(2)
-home_value = col1.number_input("Deposit",min_value=0, value=300000)
+home_value = col1.number_input("Home Value",min_value=0, value=300000)
 deposit = col1.number_input("Deposit",min_value=0, value=100000)
 interest_rate = col2.number_input("Interest Rate(in%)", min_value=0.0, value=4.0)
 loan_term = col2.number_input("Loan Term(in years)", min_value=1, value=20)
